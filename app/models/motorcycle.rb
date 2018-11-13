f527386a5057ea661918a90882fb89e5ed198f61
Anders Fleck <andersfleck@Anderss-Air.station>
2018-11-13 21:37:09 +0100

    deploy on heroku

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,3 +1,15 @@
 class Motorcycle < ApplicationRecord
   has_many :bookings
+
+  # #PSEUDO CODE FOR PRICE CALCULATION
+  # booking_days = booking.end_date - booking.start_date
+
+  # if booking_days <= 10
+  #   motorcycle.price = booking_days * 15
+  # elsif booking_days >= 10 && booking_days < 15 
+  #   motorcycle.price = booking_days * 10
+  # else
+  #   motorcycle.price = booking_days * 5
+  # end
+
 end
