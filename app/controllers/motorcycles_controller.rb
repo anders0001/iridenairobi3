b3c7cd8f22072b31dde8acb9c4cedffeb904d0fc
Anders Fleck <andersfleck@Anderss-MacBook-Air.local>
2018-11-17 15:22:57 +0100

    homepage, navbar, footer, and dashboard layout edited

--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -4,7 +4,7 @@ class MotorcyclesController < ApplicationController
       @motorcycles = []
       a = Date.parse(params["search"]["starts_at"])
       b = Date.parse(params["search"]["ends_at"])
-
+raise
       Motorcycle.all.each do |motorcycle|
         motorcycle.bookings.each do |booking|
           c = booking.start_date
